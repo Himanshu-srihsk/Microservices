599dbf05e492eb93fb66f7639f4a3abea01d89d3
Kumar Himanshu (CSS ICW ENG SW BTFW SVT) <Himanshu.Kumar@infineon.com>
2024-04-27 12:53:05 +0530

    added retry option using resilience4J

--- UserService/src/main/java/com/example/user/service/MicroUserService/controllers/UserController.java
+++ UserService/src/main/java/com/example/user/service/MicroUserService/controllers/UserController.java
@@ -19,6 +19,7 @@ import com.example.user.service.MicroUserService.entities.User;
 import com.example.user.service.MicroUserService.services.UserService;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
 @RequestMapping("/users")
@@ -34,15 +35,23 @@ public class UserController {
 	  User createdUser= userService.saveUser(user);
 	  return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
   }
+	int retryCount=1;
+	
 	@GetMapping("/{userId}")
-	@CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
+	//@CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
+	@Retry(name = "ratingHotelService", fallbackMethod = "ratingHotelFallback")
 	public ResponseEntity<User> getSingleUser(@PathVariable String userId){
+		logger.info("Get Single user Handler: user Controller");
+		logger.info("Retry Count: {}", retryCount);
+		retryCount++;
+		
 		User user= userService.getUser(userId);
 		return ResponseEntity.ok(user);
 	}
 	
 	public ResponseEntity<User> ratingHotelFallback(String userId, Exception ex){
 		logger.info("Falback is executed because service is down!!", ex.getMessage());
+		
 	    User user=	
 	    		User.builder()
 	    		.email("dummy@gmail.com")
